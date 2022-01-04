434043692fbf6351704fea85b4cc946ccf3d8dae
boodscode237 <65740750+boodscode237@users.noreply.github.com>
2022-01-04 22:03:22 +0500

    Add project files.

---
+++ ConsoleUI/ConsoleUI.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    // ELSE IF LADDER
+    int day;
+    std::cout << "Enter day number: ";
+
+    std::cin >> day;
+
+    if (day == 1)
+    {
+        std::cout << "Mon" << std::endl;
+    }
+    else if(day == 2)
+    {
+        std::cout << "Tue" << std::endl;
+    }
+    else if (day == 3)
+    {
+        std::cout << "Wed" << std::endl;
+    }
+    else if (day == 4)
+    {
+        std::cout << "Thur" << std::endl;
+    }
+    else if (day == 5)
+    {
+        std::cout << "Fri" << std::endl;
+    }
+    else if (day == 6)
+    {
+        std::cout << "Sat" << std::endl;
+    }
+    else if (day == 7)
+    {
+        std::cout << "Sun" << std::endl;
+    }
+    else
+    {
+        std::cout << "Invalid day" << std::endl;
+    }
+
+
+}
